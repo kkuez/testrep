f6dfa27fbd0147ee841adfb5fc04812e70ca663d
Musikbib <Musikbib@musikbibs-mac-mini.musik.hf.uni-koeln.de>
2018-09-11 13:42:46 +0200

    Code eingefügt in main Klasse, nur testweise

--- Testapp/src/main/java/com/mycompany/app/App.java
+++ Testapp/src/main/java/com/mycompany/app/App.java
@@ -1,4 +1,8 @@
+
 package com.mycompany.app;
+import java.util.Scanner;
+
+
 
 /**
  * Hello world!
@@ -8,6 +12,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+	
+
+        System.out.println( "Nachricht eingeben:" );
+	Scanner ein= new Scanner(System.in);
+	String einstr= ein.next();
+	System.out.println(einstr);
     }
 }
